7e13e0ed049c620919c8aca0a7ef95aac710ed4d
Honeytang-02 <jw9381@hknu.ac.kr>
2025-09-30 14:26:33 +0900

    코드 수정

--- main_print_v1.py
+++ main_print_v1.py
@@ -1,11 +1,11 @@
 # 문자열을 출력합니다.
-print("Hello, Honeytang-02")
+print("Hello, 재원")
 
 # 숫자를 출력합니다.
 print(99999)
 
 # 변수를 선언하고 출력합니다.
-name = "Honeytang"
+name = "재원"
 age = 24
 print(name)
 print(age)
@@ -13,3 +13,7 @@ print(age)
 # 여러 값을 쉼표(,)로 구분하여 한 줄에 출력합니다.
 print("이름:", name, "나이:", age)
 
+
+print("집 가고싶다")
+
+print("왜 안바뀌노 추가 했자너")
