97e4e1d5d70b148694a4fd4e50deb20457e2399c
Honeytang-02 <jw9381@hknu.ac.kr>
2025-09-30 14:15:49 +0900

    second commit

---
+++ main_print_v1.py
@@ -0,0 +1,15 @@
+# 문자열을 출력합니다.
+print("Hello, Honeytang-02")
+
+# 숫자를 출력합니다.
+print(99999)
+
+# 변수를 선언하고 출력합니다.
+name = "Honeytang"
+age = 24
+print(name)
+print(age)
+
+# 여러 값을 쉼표(,)로 구분하여 한 줄에 출력합니다.
+print("이름:", name, "나이:", age)
+
